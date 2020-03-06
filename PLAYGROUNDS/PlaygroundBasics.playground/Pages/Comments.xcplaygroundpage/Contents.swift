9171d4639b35058e0db8d541d3e777f5929f7416
Taimoor <744255@pdsb.net>
2020-03-06 16:34:05 -0500

    ADDED PLAYGROUND

---
+++ PLAYGROUNDS/PlaygroundBasics.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: ## Comments
+//: Programmers leave little notes right in the code to help explain it.
+//: This helps people understand the code when they read it later.
+//: These notes are called _comments_.
+//:
+//: Comments are ignored by the playground and do not affect how your code is run.
+//: 
+//: A comment starts with two slashes `//` :
+// This is a comment; it does not affect the code
+34 + 56 + 230
+//: Because comments are ignored, no result is displayed in the results bar for that line of code.
+// 200
+print("hi")
