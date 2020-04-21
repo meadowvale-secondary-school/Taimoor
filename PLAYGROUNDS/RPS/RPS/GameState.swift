273a784c3ce1560fcb6504bb583b08afe38cb2ac
Taimoor <744255@pdsb.net>
2020-04-21 17:32:02 -0400

    Committed lesson 20

---
+++ PLAYGROUNDS/RPS/RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Student on 2020-04-16.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
