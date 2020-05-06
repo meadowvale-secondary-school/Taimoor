4e62c9719664a608dbc0efd40a724d6e6609256e
Taimoor <744255@pdsb.net>
2020-05-05 23:01:58 -0400

    Redid lesson 2

--- Student Resources 2/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Student Resources 2/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,6 +3,38 @@
  
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
+/*:
+ ## Exercise - Create Functions
+ 
+ Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
+ */
+func introduceMyself() {
+    print("Hi! My name is Taimoor. I like cars.")
+}
+introduceMyself()
+/*:
+ Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
+ */
+func magicEightBall(){
+    let randomNum = Int.random (in:0...4)
+    switch randomNum {
+    case 0:
+        print("You got \(randomNum). You came last.")
+    case 1:
+        print("You got \(randomNum). You are now number 1!.")
+    case 2:
+        print("You came second.")
+    case 3:
+        print("You came third.")
+    case 4:
+        print("You came fourth")
+    default:
+    print("Try again")
+    }
+}
+magicEightBall()
+
+//: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
 
 
 /*:
